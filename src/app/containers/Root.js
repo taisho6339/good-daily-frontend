3212e040b157638b5c79397a059a8ef563ad4f4f
坂本大将 <taisho6339@sakamototaishou-no-MacBook-Pro.local>
2017-10-22 23:35:39 +0900

    色を定数化

--- src/app/containers/Root.js
+++ src/app/containers/Root.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import AppBar from 'react-toolbox/lib/app_bar';
 import './Root.scss';
 
 function Root(props) {
   return (
-    <div>
+    <div styleName="application">
+      <AppBar title="React" />
       {props.children}
     </div>
   );
